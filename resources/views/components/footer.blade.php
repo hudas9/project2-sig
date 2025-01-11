19d2581f009ddc26397e46bf023ebb505f28f14b
Ahmad Huda Salam <ahmadhudasalam9@gmail.com>
2025-01-11 11:17:46 +0700

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
-                <p>Dibuat tanpa ❤️ and ☕ - <a href="https://github.com/hudas9/" target="_blank"> Ahmad Huda Salam</a>
+                <p>Dibuat tanpa ❤️ dan ☕ - <a href="https://github.com/hudas9/" target="_blank"> Ahmad Huda Salam</a>
                     & Team &copy; {{ date('Y') }} <a href="https://github.com/hudas9/project2-sig" target="_blank"
                         class="text-white font-weight-bold">SIG
                         SulSel</a></p>
